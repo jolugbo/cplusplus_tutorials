78a3e2bb316dcd3b2d2ad864a643803ea5cf19f2
jolugbo olufemi <jolugbofemi@gmail.com>
2021-10-29 19:42:19 -0700

    currently implementing list reversal

--- cPlusPlusTutorial/data_structure/LinkedList/LinkedListRecursive.cpp
+++ cPlusPlusTutorial/data_structure/LinkedList/LinkedListRecursive.cpp
@@ -11,30 +11,79 @@ public:
 		Node n3(3);
 		Node n4(4);
 		Node n5(5);
+		Node n6(6);
 		Node* randomNode = new Node(455);
 		head = &n1;
 		n1.next = &n2;
 		n2.next = &n3;
 		n3.next = &n4;
 		n4.next = &n5;
+		n5.next = &n6;
 		int len = length(head);
-		cout << FindItemInLinkedList(head,4,0);
+		cout << getMidNode(head)->data;
 	}
-	int FindItemInLinkedList(Node* node, int item,int position) {
+	Node* getMidNode(Node* node) {
+		int len = length(node);
+		int mid = len / 2;
+		int counter = 0;
+		while (counter < mid)
+		{
+			node = node->next;
+			counter++;
+		}
+		return node;
+	}
+	Node* reverseList(Node* node) {
+		Node* temp = new Node(node->data);
+		while (node -> next)
+		{
+			temp = node->next;
+			node->next = node;
+
+		}
+	}
+	//slow fast approach
+	Node* getMidNode2(Node* node) {
+		Node* slow = node;
+		Node* fast = node->next;
+		while (fast && fast->next)
+		{
+			slow = slow->next;
+			fast = fast->next->next;
+		}
+		if (fast)
+		{
+			return slow->next;
+		}
+		return slow;
+	}
+	bool FindItemInLinkedList(Node* node, int item) {
 		if (node == NULL)
 		{
-			return -1;
+			return false;
+		}
+		if (node->data == item)
+		{
+			return true;
+		}
+		return FindItemInLinkedList(node->next, item);
+	}
+	bool FindItemInLinkedList2(Node* node, int item) {
+		if (node == NULL)
+		{
+			return false;
 		}
 		else
 		{
-			if (node->data == item)
+			while (node->next != NULL)
 			{
-				return position + 1;
+				if (node->data == item)
+				{
+					return true;
+				}
+				node = node->next;
 			}
-			else
-			{
-				return FindItemInLinkedList(node->next,item, position + 1);
-			}
+			return false;
 		}
 	}
 	int length(Node* node) {
@@ -44,7 +93,7 @@ public:
 		}
 		else
 		{
-			node = node -> next;
+			node = node->next;
 			return 1 + length(node);
 		}
 	}
