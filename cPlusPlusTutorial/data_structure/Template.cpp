2cacdaa7579e10c628328958b2e22b0822deda70
jolugbo olufemi <jolugbofemi@gmail.com>
2021-11-06 08:39:30 +0100

    template use

---
+++ cPlusPlusTutorial/data_structure/Template.cpp
@@ -0,0 +1,18 @@
+template<typename T>
+class Pair {
+	T x;
+	T y;
+	T getX() {
+		return x;
+	}
+	T getY() {
+		return y;
+	}
+	void setX(T inX) {
+		x = inX;
+	}
+	void setY(T inY) {
+		y = inY;
+	}
+
+};
