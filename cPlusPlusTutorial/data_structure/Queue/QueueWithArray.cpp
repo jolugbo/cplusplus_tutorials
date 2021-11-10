8353a6f1e2d618c998cc689a38d2d402e22e4ed5
jolugbo olufemi <jolugbofemi@gmail.com>
2021-11-10 21:09:13 +0100

    Queue With Array to be continued, electricity issues

--- cPlusPlusTutorial/data_structure/Queue/QueueWithArray.cpp
+++ cPlusPlusTutorial/data_structure/Queue/QueueWithArray.cpp
@@ -1,3 +1,20 @@
 class QueueWithArray {
+public:
+	int firstIndex = 0;
+	int currentIndex = 0;
+	int size = 0;
+	int width = 0;
+	int* arr;
+	QueueWithArray(int len) {
+		width = len;
+		arr = new int[width];
+	}
 
+	void push(int value) {
+		arr[currentIndex++] = value;
+	}
+	void pop() {
+		if (firstIndex != 0)
+			firstIndex++;
+	}
 };
