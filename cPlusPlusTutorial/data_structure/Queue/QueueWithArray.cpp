ceed8a4bb9d5d47ca4b511477e8b308b69f8af40
jolugbo olufemi <jolugbofemi@gmail.com>
2021-11-11 23:28:43 +0100

    Queue With Array updated

--- cPlusPlusTutorial/data_structure/Queue/QueueWithArray.cpp
+++ cPlusPlusTutorial/data_structure/Queue/QueueWithArray.cpp
@@ -1,36 +1,56 @@
 #include <cstdlib>
+#include <iostream>
+using namespace std;
+template<typename T>
 class QueueWithArray {
+	T x;
 public:
 	int firstIndex = 0;
 	int currentIndex = 0;
 	int size = 0;
 	int width = 0;
-	int* arr;
+	T* arr;
 	QueueWithArray(int len) {
 		width = len;
 		arr = new int[width];
 	}
 
-	void push(int value) {
+	void push(T value) {
+		if (size == width)
+		{
+			cout << "queue filled" << endl;
+		}
 		if (currentIndex == size)
 		{
 			currentIndex = 0;
 			arr[currentIndex++] = value;
 		}
-		arr[currentIndex++] = value;
+		else {
+			arr[currentIndex++] = value;
+		}
+		size++;
 	}
 	void pop() {
-		if (firstIndex != 0)
+		if (firstIndex != size)
 			firstIndex++;
+		else
+		{
+			firstIndex = ++firstIndex % width;
+		}
+		size--;
+
 	}
 	int front() {
 		return arr[firstIndex];
-	}
+	} 
 	int size() {
-		return abs(currentIndex - firstIndex - 1);
+		return size;
+	}
+	bool isEmpty() {
+		return size == 0;
 	}
 };
 
 // 1 2 3 4 5
 // 1 0 0 1 1
-// 1 1 0 0 0
+// 1 1 0 1 1  2 - 4
