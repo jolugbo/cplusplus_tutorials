d750ab9e221e001e023429a93d53eeb5b52c1b1f
jolugbo olufemi <jolugbofemi@gmail.com>
2021-12-21 13:23:38 +0100

    hash map structure created

--- cPlusPlusTutorial/data_structure/HashMap/HashMap.cpp
+++ cPlusPlusTutorial/data_structure/HashMap/HashMap.cpp
@@ -1,12 +1,52 @@
 using namespace std;
 template<typename V>
-
 class HashMapNode {
+public:
 	string key;
 	V value;
 	HashMapNode* next;
+	HashMapNode(string key V value) {
+		this->key = key;
+		this->value = value;
+		next = NULL;
+	}
+	~HashMapNode() {
+		delete next;
+	}
 };
+
+template<typename V>
 class HashMap {
+private:
+	HashMapNode<V>** buckets;
+	int count;
+	int numBuckets;
+public:
+	HashMap() {
+		count = 0;
+		numBucket = 5;
+		buckets = new HashMapNode<V>*[numBuckets];
+	}
+	~HashMap() {
+		for (int i = 0; i < numBuckets; i++)
+		{
+			delete buckets[i];
+		}
+		delete []buckets;
+	}
+	int size() {
+		return count;
+	}
 
+	V getValue(string key) {
+	
+	}
 
+	void insert(string key, V value) {
+	
+	}
+
+	void remove(string key) {
+	
+	}
 };
