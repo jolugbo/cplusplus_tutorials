b11fc069a7f0e45f93ea153f4b8be2cd93d41d87
jolugbo olufemi <jolugbofemi@gmail.com>
2022-01-27 02:50:50 +0100

    Kruskal started

--- cPlusPlusTutorial/data_structure/Graphs/Kruskal.cpp
+++ cPlusPlusTutorial/data_structure/Graphs/Kruskal.cpp
@@ -3,7 +3,19 @@ using namespace std;
 //for solving minimum spanning tree problem
 class Kruskal {
 public:
-	Kruskal(vector<vector<int>>matrix) {
+	Kruskal(vector<vector<int>>  matrix) {
 
+	}
+
+	void MakeSet() {
+	
+	}
+
+	void Union() {
+	
+	}
+
+	void Find() {
+	
 	}
 };
