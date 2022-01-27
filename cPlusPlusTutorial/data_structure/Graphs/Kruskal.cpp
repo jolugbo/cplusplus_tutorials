ce24a4fa3b27c4558c710f20091757140919342a
jolugbo olufemi <jolugbofemi@gmail.com>
2022-01-27 04:51:28 +0100

    Kruskal updated

--- cPlusPlusTutorial/data_structure/Graphs/Kruskal.cpp
+++ cPlusPlusTutorial/data_structure/Graphs/Kruskal.cpp
@@ -2,20 +2,27 @@
 using namespace std;
 //for solving minimum spanning tree problem
 class Kruskal {
+	vector<vector<int>>  Graph;
+	int* parent;
 public:
 	Kruskal(vector<vector<int>>  matrix) {
-
+		Graph = matrix;
 	}
 
-	void MakeSet() {
-	
+	void MakeSet(int i) {
+		parent[i] = i;
 	}
 
-	void Union() {
-	
+	void Union(int x, int y) {
+		int xParent = Find(x);
+		int yParent = Find(y);
+		parent[yParent] = xParent;
 	}
 
-	void Find() {
-	
+	int Find(int i) {
+		while (parent[i] != i) {
+			i = parent[i];
+		}
+		return i;
 	}
 };
