87541b7357ee65d05a4119e5e15f396e0be9e896
jolugbo olufemi <jolugbofemi@gmail.com>
2022-03-29 08:05:11 +0100

    CommutableIsland still buggy

--- cPlusPlusTutorial/data_structure/Graphs/CommutableIsland.cpp
+++ cPlusPlusTutorial/data_structure/Graphs/CommutableIsland.cpp
@@ -1,33 +1,33 @@
 #include <vector>
 #include <iostream>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
 class CommutableIsland {
 public:
 	//to be cont'd
+	static bool compareInterval(vector<int>& v1, const vector<int>& v2)
+	{
+		return (v1[2] < v2[2]);
+	}
+
 	CommutableIsland(int A, vector<vector<int> >& B) {
 		vector<vector<int> > output;
 		unordered_map<int, vector<int>> route_path_map;
 		int totalCost = 0;
-		for (int i = 1; i <= A; i++)
-		{
-			vector<int> islandDetails = { i, 0, INT32_MAX };
-			output.push_back(islandDetails);
-		}
-		for (int i = 0; i < B.size(); i++)
+		sort(B.begin(), B.end(), compareInterval);
+		for (int i = 0; i < A - 1; i++)
 		{
 			vector<int> islandDetails = B[i];
+			totalCost += islandDetails[2];
 			int originIsland = islandDetails[0] - 1;
 			int destinationIsland = islandDetails[1] - 1;
 			int journeyCost = islandDetails[2];
-			if (output[destinationIsland][2] > journeyCost) {
-				output[destinationIsland][1] = originIsland;
-				output[destinationIsland][2] = journeyCost;
-			}
+			cout << islandDetails[0] << " " << islandDetails[1] << " " << islandDetails[2] <<endl;
 		}
-		for (int i = 1; i < A; i++)
+		/*for (int i = 1; i < A; i++)
 		{
 			totalCost+=output[i][2];
-		}
+		}*/
 	}
 };
