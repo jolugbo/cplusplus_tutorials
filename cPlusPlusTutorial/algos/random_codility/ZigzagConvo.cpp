10415ce1f349b245ecd48c1d12760c404abbab1c
jolugbo olufemi <jolugbofemi@gmail.com>
2022-04-19 19:05:35 +0100

    Zigzag Convo started

---
+++ cPlusPlusTutorial/algos/random_codility/ZigzagConvo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+class ZigzagConvo {
+public:
+	ZigzagConvo(string s, int numRows) {
+		string returnString;
+		for (int i = 0; i < s.length(); i++)
+		{
+
+		}
+	}
+};
