bf0d5f9179c786c644480fc17bb5258ccbf12dc3
jolugbo olufemi <jolugbofemi@gmail.com>
2022-05-04 12:45:28 +0100

    RegularExpressionMatching implemented using recurssion

--- cPlusPlusTutorial/algos/random_codility/RegularExpressionMatching.cpp
+++ cPlusPlusTutorial/algos/random_codility/RegularExpressionMatching.cpp
@@ -7,11 +7,33 @@ public:
 		int lenS = s.size(), lenP = p.size();
 
 		vector<vector<int>> dp(lenS + 1, vector<int>(lenP + 1,-1));
-		cout << matchFound(s, p, 0, 0, lenS, lenP, dp);
+		cout << matchFound(s, p, lenS, lenP);
 	}
 
-	bool matchFound(string s, string p, int start, int end, int lenS, int lenP, vector<vector<int>> dp) {
-		if (start >= lenS && end >= lenP)return true;
+	bool matchFound(string s, string p, int lenS, int lenP) {
+		if (lenP == 0)
+		{
+			return lenS == 0;
+		}
+		if (lenP > 1 && p[1] == '*')
+		{
+			if (matchFound(s,p.substr(2),s.length(),p.substr(2).length()))
+			{
+				return true;
+			}
+			if ((s[0] == p[0] || p[0] == '.') && lenS > 0) {
+				return matchFound(s.substr(1), p, s.substr(1).length(), p.length());
+			}
+			return false;
+		}
+		else
+		{
+			if ((s[0] == p[0] || p[0] == '.') && lenS > 0) {
+				return matchFound(s.substr(1), p.substr(1), s.substr(1).length(), p.substr(1).length());
+			}
+			return false;
+		}
+		/*if (start >= lenS && end >= lenP)return true;
 		if (end >= lenP)return false;
 		if (dp[start][end] != -1) return dp[start][end];
 		bool match = start < lenS and (s[start] == p[end] or p[end] == '.');
@@ -20,7 +42,7 @@ public:
 			return dp[start][end] = matchFound(s, p, start, end + 2, lenS, lenP, dp) or matchFound(s, p, start + 1, end, lenS, lenP, dp);
 		}
 		if(match) return dp[start][end] = matchFound(s, p, start + 1, end + 1, lenS, lenP, dp);
-		return dp[start][end] = false;
+		return dp[start][end] = false;*/
 		/*if (start == lenS && end == lenP)return true;
 		if (end == lenP) return false;
 		if (start == lenS)
