3546cad73009c38979a34d6fbcb45af095465ede
jolugbo olufemi <jolugbofemi@gmail.com>
2022-05-05 03:14:16 +0100

    ContainerWithMostWater setting up DP data structure

--- cPlusPlusTutorial/algos/random_codility/ContainerWithMostWater.cpp
+++ cPlusPlusTutorial/algos/random_codility/ContainerWithMostWater.cpp
@@ -3,6 +3,12 @@ using namespace std;
 class ContainerWithMostWater {
 public:
 	ContainerWithMostWater(vector<int>& height) {
-	
+		const int size = 5;// height.size();
+		vector<vector<int>> dp;
+		for (int i = 0; i < height.size(); i++)
+		{
+			vector<int> row(height.size());
+			dp.push_back(row);
+		}
 	}
 };
