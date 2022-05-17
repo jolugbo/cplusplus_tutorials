c61584e9d8f8ac96cd9775b532f01f3dddf7e865
jolugbo olufemi <jolugbofemi@gmail.com>
2022-05-17 10:32:03 +0100

    GenerateParenthesis first attempt

--- cPlusPlusTutorial/algos/dynamic_programming/GenerateParenthesis.cpp
+++ cPlusPlusTutorial/algos/dynamic_programming/GenerateParenthesis.cpp
@@ -1,6 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 class GenerateParenthesis {
 public:
-	GenerateParenthesis() {
-	
+	GenerateParenthesis(int n) {
+		vector<string> generatedList;
+		string current = "";
+		generatorUtil(n, 0, 0, current, generatedList);
+		
+	}
+	void generatorUtil(int n,int openCount,int closedCount,string current,vector<string> generatedList) {
+		if (current.length() == (2 *n))
+		{
+			generatedList.push_back(current);
+			current = "";
+			return;
+		}
+		if (openCount < n) {
+			current += '(';
+			generatorUtil(n, 1 + openCount, closedCount, current, generatedList);
+			current.pop_back();
+		}
+		if (closedCount < n) {
+			current += ')';
+			generatorUtil(n, openCount, 1 + closedCount, current, generatedList);
+			current.pop_back();
+		}
 	}
 };
