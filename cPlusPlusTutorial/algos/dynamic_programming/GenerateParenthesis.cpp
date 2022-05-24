f5e216742344ba1a7f67f7199d4579c3f42dda87
jolugbo olufemi <jolugbofemi@gmail.com>
2022-05-24 15:20:11 +0100

    revisited generate Parenthesis

--- cPlusPlusTutorial/algos/dynamic_programming/GenerateParenthesis.cpp
+++ cPlusPlusTutorial/algos/dynamic_programming/GenerateParenthesis.cpp
@@ -6,25 +6,28 @@ public:
 	GenerateParenthesis(int n) {
 		vector<string> generatedList;
 		string current = "";
-		generatorUtil(n, 0, 0, current, generatedList);
-		
+		generatorUtil(n, 0, 0, current, generatedList);	
 	}
-	void generatorUtil(int n,int openCount,int closedCount,string current,vector<string> generatedList) {
-		if (current.length() == (2 *n))
-		{
-			generatedList.push_back(current);
-			current = "";
+	void generatorUtil(int n, int openCount, int closedCount,string current,vector<string>& bucket) {
+		if (current.size() == (2 * n)) {
+			if (openCount == n and closedCount == n)
+			{
+				bucket.push_back(current);
+			}
 			return;
 		}
-		if (openCount < n) {
-			current += '(';
-			generatorUtil(n, 1 + openCount, closedCount, current, generatedList);
+		if (openCount < n)
+		{
+			current+='(';
+			generatorUtil(n, openCount + 1, closedCount, current, bucket);
 			current.pop_back();
 		}
-		if (closedCount < n) {
+		if (closedCount < openCount)
+		{
 			current += ')';
-			generatorUtil(n, openCount, 1 + closedCount, current, generatedList);
+			generatorUtil(n, openCount, closedCount + 1, current, bucket);
 			current.pop_back();
 		}
 	}
+	
 };
