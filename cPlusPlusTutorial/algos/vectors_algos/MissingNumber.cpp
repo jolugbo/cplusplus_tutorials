1767cb3bc4e979d8374b128715198e8dfaa48013
jolugbo olufemi <jolugbofemi@gmail.com>
2022-05-28 22:43:22 +0100

    Daily challenger Missing Number completed

--- cPlusPlusTutorial/algos/vectors_algos/MissingNumber.cpp
+++ cPlusPlusTutorial/algos/vectors_algos/MissingNumber.cpp
@@ -1,9 +1,26 @@
 #include <vector>
 #include <algorithm>
+#include <iostream>
 using namespace std;
 class MissingNumber {
 public:
      MissingNumber(vector<int>& nums) {
-
+         cout<< calc(nums);
     }
+     int calc(vector<int>& nums) {
+         if (nums.size() == 1 and nums[0] == 0) return 1;
+         if (nums.size() == 1 and nums[0] == 1) return 0;
+         sort(nums.begin(), nums.end());
+         int resp = 0;
+         for (int i = 0; i < nums.size(); i++)
+         {
+             if (i != nums[i])
+             {
+                 return i;
+             }
+         }
+         if ((resp == 0 and nums.size() != 1))return nums.size();
+         else
+             return resp;
+     }
 };
