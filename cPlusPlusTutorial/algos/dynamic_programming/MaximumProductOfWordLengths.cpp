5fe705e95fbb4cff66bee7eb2bcae96bd1b76de1
jolugbo olufemi <jolugbofemi@gmail.com>
2022-05-29 22:24:50 +0100

    solved MaximumProductOfWordLengths

--- cPlusPlusTutorial/algos/dynamic_programming/MaximumProductOfWordLengths.cpp
+++ cPlusPlusTutorial/algos/dynamic_programming/MaximumProductOfWordLengths.cpp
@@ -8,17 +8,36 @@ public:
 	MaximumProductOfWordLengths(vector<string>& words) {
 		vector<int > memoryPorts(words.size() + 1, 0);
 		vector< vector<int>> computerMemory;
-		for (int i = 0; i < words.size(); i++)
+		for (int i = 0; i < words.size() + 1; i++)
 		{
 			computerMemory.push_back(memoryPorts);
 		}
-		for (int i = words.size() + 1; i >= 0; i--)
+		for (int i = words.size(); i >= 0; i--)
 		{
 
-			for (int j = words.size() + 1; j >= 0; j--)
+			for (int j = words.size(); j >= 0; j--)
 			{
-
+				if (i >= words.size() or j >= words.size()) {
+					computerMemory[i][j] = 0;
+				}
+				else if (shareCommonLetters(words[i],words[j])) {
+					computerMemory[i][j] = max(computerMemory[i + 1][j], computerMemory[i][j + 1]);
+				}
+				else {
+					int size = words[i].length() * words[j].length();
+					computerMemory[i][j] = max(size, max(computerMemory[i + 1][j], computerMemory[i][j + 1]));
+				}
 			}
 		}
+		cout << computerMemory[0][0];
+	}
+	bool shareCommonLetters(string first, string second) {
+		for (int i = 0; i < first.length(); i++)
+		{
+			if (second.find(first[i]) != string::npos) {
+				return true;
+			};
+		}
+		return false;
 	}
 };
