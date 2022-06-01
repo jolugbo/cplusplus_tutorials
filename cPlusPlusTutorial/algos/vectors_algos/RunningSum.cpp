1dd3a43f6e81b3d543f386d4d0d2173c283ed75f
jolugbo olufemi <jolugbofemi@gmail.com>
2022-06-01 12:14:01 +0100

    first attempt RunningSum

--- cPlusPlusTutorial/algos/vectors_algos/RunningSum.cpp
+++ cPlusPlusTutorial/algos/vectors_algos/RunningSum.cpp
@@ -5,6 +5,12 @@ using namespace std;
 class RunningSum {
 public:
 	RunningSum(vector<int> nums) {
-		
+		int len = nums.size();
+		vector<int> resp(len);
+		resp[0] = nums[0];
+		for (int i = 1; i < len; i++)
+		{
+			resp[i] = resp[i - 1] + nums[i];
+		}
 	}
 };
