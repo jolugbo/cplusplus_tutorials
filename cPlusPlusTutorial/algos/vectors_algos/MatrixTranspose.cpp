bd3312968eaa91c7187dccc65944a98ce5e2f8fc
jolugbo olufemi <jolugbofemi@gmail.com>
2022-06-02 18:27:53 +0100

    completed MatrixTranspose

--- cPlusPlusTutorial/algos/vectors_algos/MatrixTranspose.cpp
+++ cPlusPlusTutorial/algos/vectors_algos/MatrixTranspose.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class MatrixTranspose {
 public:
-	MatrixTranspose() {
-		
+	MatrixTranspose(vector<vector<int>>& matrix) {
+		int len = matrix.size();
+		int size = matrix[0].size();
+		vector<vector<int>> resp;
+		for (int i = 0; i < size; i++)
+		{
+			vector<int> row;
+			for (int j = 0; j < matrix.size(); j++)
+			{
+					row.push_back(matrix[j][i]);
+			}
+			resp.push_back(row);
+		}
+
 	}
 };
