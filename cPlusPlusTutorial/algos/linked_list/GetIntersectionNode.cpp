7ea48c37c4ec41a8c5272733d251b324cff50e62
jolugbo olufemi <jolugbofemi@gmail.com>
2022-06-08 16:02:16 +0100

    GetIntersectionNode solved

--- cPlusPlusTutorial/algos/linked_list/GetIntersectionNode.cpp
+++ cPlusPlusTutorial/algos/linked_list/GetIntersectionNode.cpp
@@ -5,22 +5,53 @@ using namespace std;
 class GetIntersectionNode {
 public:
 	GetIntersectionNode(Node* headA, Node* headB) {
-		Node responseNode = NULL;
-		Node* temp = new Node(headA->data);
-		Node* temp1 = new Node(headB->data);
-		while (headA->next)
-		{
-			headA = headA->next;
-			Node* holderNode = new Node(headA->data);
-			holderNode->next = temp;
-			temp = holderNode;
+
+		Node* temp1 = headA, * temp2 = headB;
+		while (temp1 != temp2) {
+			if (temp1)temp1 = temp1->next;
+			else temp1 = headB;
+			if (temp2)temp2 = temp2->next;
+			else temp2 = headA;
 		}
-		while (headB->next)
-		{
-			headB = headB->next;
-			Node* holderNode = new Node(headB->data);
-			holderNode->next = temp1;
-			temp1 = holderNode;
-		}
+		//return temp2;
+		cout<< "done";
+
+
+		//Node responseNode = NULL;
+		//Node* temp = new Node(headA->data);
+		//Node* temp1 = new Node(headB->data);
+		//Node* outputNode = new Node(headB->data);
+		//Node* outputNode2 = outputNode;
+		//while (headA->next)
+		//{
+		//	headA = headA->next;
+		//	Node* holderNode = new Node(headA->data);
+		//	holderNode->next = temp;
+		//	temp = holderNode;
+		//}
+
+		//while (headB->next)
+		//{
+		//	headB = headB->next;
+		//	Node* holderNode = new Node(headB->data);
+		//	holderNode->next = temp1;
+		//	temp1 = holderNode;
+		//}
+
+		//while (temp->next or temp1->next)
+		//{
+		//	if (temp->data == temp->data)
+		//	{
+		//		Node* holderNode = new Node(temp->next->data);
+		//		outputNode->next = holderNode;
+		//		outputNode = outputNode->next;
+		//		temp = temp->next;
+		//		temp1 = temp1->next; 
+		//	}
+		//	else
+		//	{
+		//		//return outputNode;
+		//	}
+		//}
 	}
 };
