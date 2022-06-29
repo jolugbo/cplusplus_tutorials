6ab805a53c570c06b879a251890cd83659aa9c90
jolugbo olufemi <jolugbofemi@gmail.com>
2022-06-30 00:57:56 +0100

    string converted to numbers(stream shortcut)

--- cPlusPlusTutorial/algos/amazon/MyAtoi.cpp
+++ cPlusPlusTutorial/algos/amazon/MyAtoi.cpp
@@ -1,10 +1,12 @@
 //#include <iostream>
 #include <sstream>
+#include <regex>
 using namespace std;
 
 class MyAtoi {
 public:
 	MyAtoi(string s) {
+		regex_replace(s, regex("(^[ ]+)|([ ]+$)"), "");
 		stringstream holder(s);
 		int output = 0;
 		holder >> output;
