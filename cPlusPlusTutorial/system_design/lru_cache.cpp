429a1abc082e8599b5383f8d29c6588f60e0c0e1
jolugbo olufemi <jolugbofemi@gmail.com>
2022-07-02 20:09:40 +0100

    completed threeSum

---
+++ cPlusPlusTutorial/system_design/lru_cache.cpp
@@ -0,0 +1,17 @@
+#include <unordered_map>;
+#include <stack>
+using namespace std;
+class lru_cache {
+public:
+	unordered_map<int, int> pair;
+	stack<int> list;
+	lru_cache(int capacity) {
+		//list.
+	}
+	int get(int key) {
+	
+	}
+	void put(int key, int value) {
+	
+	}
+};
